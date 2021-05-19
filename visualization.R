ab6f00612c1badd3566300cf98c745ccbc77c153
zogak <zogak7@gmail.com>
2021-05-19 20:47:02 +0900

    graphs

--- visualization.R
+++ visualization.R
@@ -8,22 +8,32 @@ targetData <- first_data %>% full_join(second_data, by=c('App'='name'))
 View(targetData)
 
 #for whole data
-#1.relation between SIZE and INSTALLS
-ggplot(data=targetData, aes(Size, Installs)) + geom_point()
+#review and rating
+ggplot(data=targetData, aes(Reviews, Rating, color=Type)) + geom_point(size = 2, alpha=0.5)+scale_x_log10()
+
+#relation between SIZE and INSTALL
+ggplot(data=targetData, aes(Size, Installs)) + geom_line()
+
+#type and rating
+ggplot(data=targetData, aes(Type, Rating)) + geom_boxplot(width = 0.3)
 
 #by group
-#Installs by Category
+#category & install
 category_install <- targetData%>%group_by(Category)%>%summarize(mean_Installs = mean(Installs))
 CI <- category_install
 #sort
 CI$Category <- factor(CI$Category, levels=CI$Category[order(CI$mean_Installs)])
 ggplot(data=CI, aes(x = mean_Installs, y = Category)) + geom_bar(stat = 'identity')
 
-#table(targetData$color)
-
+#category & size
+category_size <- targetData %>% filter(!is.na(Size))
+category_size <- category_size%>%group_by(Category)%>%summarize(mean_size = mean(Size))
+CS <- category_size
+CS$Category <- factor(CS$Category, levels=CS$Category[order(CS$mean_size)])
+ggplot(data=CS, aes(x = mean_size, y = Category)) + geom_bar(stat='identity')
 #that has color
 allCol <- targetData %>% filter(!is.na(color))
-ggplot(data=allCol, aes(x = Rating, y = Category)) + geom_point(shape = 21, color = 'black', fill = allCol$color, size = 3, stroke = 0.7)
+ggplot(data=allCol, aes(x = Installs, y = Category)) + geom_point(shape = 21, color = 'black', fill = allCol$color, size = 3, stroke = 0.7) + scale_x_log10()
 
 ggplot(data=allCol, aes(x = color, y = Category)) + geom_point(size = 3, color = allCol$color)
 ggplot(data=allCol, aes(x = color, y = Category)) + geom_point(shape = 21, color = 'black', fill = allCol$color, size = 3, stroke = 0.7)
@@ -33,7 +43,19 @@ colGroup$color <- factor(colGroup$color, levels=c('red','pink','orange','yellow'
 ggplot(data=colGroup, aes(x = color, y = Category)) + geom_point(shape = 21, color = 'black', fill = allCol$color, size = 3, stroke = 0.7)
 
 
-barplot(table(allCol$color))
+#merge blue & the most used color
+blueColor <- allCol
+blueColor[blueColor$color == 'lightBlue', 'color'] <- 'blue'
+table(blueColor$color)
+barplot(sort(table(blueColor$color), decreasing = TRUE), col=c('white','darkblue','darkred','black','darkgreen','purple3','gold','pink1','orange2'))
 
+#category color
+ggplot(data=blueColor, aes(x = Installs, y = Category)) + geom_point(shape = 21, color = 'black', fill = blueColor$color, size = 3, stroke = 0.7) + scale_x_log10()
+ggplot(data=blueColor, aes(x = Reviews, y = Category)) + geom_point(shape = 21, color = 'black', fill = blueColor$color, size = 3, stroke = 0.7) + scale_x_log10()
+ggplot(data=blueColor, aes(x = Rating, y = Category)) + geom_point(shape = 21, color = 'black', fill = blueColor$color, size = 3, stroke = 0.7) + scale_x_log10()
 
-#category bul color
+View(blueColor %>% group_by(Category) %>% summarize(colorGroup = table(blueColor$color)))
+
+ggplot(data=blueColor, aes(Installs, Rating)) + geom_point(shape = 21, size = 3, fill = blueColor$color, color = 'black', stroke=0.7) + scale_x_log10()
+
+ggplot(data=blueColor, aes(Rating, Reviews)) + geom_point(size = 2, color = blueColor$color)+scale_y_log10()
